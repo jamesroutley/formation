80558048626ce8b56ba60644d006fa6586acca15
James Routley <jroutley@gmail.com>
2017-02-22 14:19:06 +0100

    Updating cfn resource specification

--- formation/__init__.py
+++ formation/__init__.py
@@ -7,7 +7,8 @@ __version__ = "0.1.0"
 
 
 RESOURCE_SPECIFICATION_PATH = os.path.join(
-    os.path.dirname(__file__), "data", "resource-specification.json"
+    os.path.dirname(__file__), "data",
+    "CloudFormationResourceSpecification.json"
 )
 
 ATTRIBUTE_SPECIFICATION_PATH = os.path.join(
