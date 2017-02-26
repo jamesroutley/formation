445f4cb53552465ee058347b078f4e57579cce3f
James Routley <jroutley@gmail.com>
2017-02-26 20:54:44 +0000

    Adding exception docstrings

--- formation/exception.py
+++ formation/exception.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 
+"""
+formation.exception implements the exceptions raised by Formation.
+
+"""
+
 
 class FormationError(Exception):
+    """
+    Formation's base exception. All other exceptions inherit from this.
+
+    """
     pass
 
 
 class InvalidPropertyError(FormationError):
+    """
+    Exception raised when user supplied properties are invalid.
+
+    """
     pass
