ebbc3968d830bbf0b22b173305cf462fde5988b9
James Routley <jroutley@gmail.com>
2017-03-02 13:42:19 +0100

    Update exception docstrings

--- formation/exception.py
+++ formation/exception.py
@@ -10,13 +10,27 @@ class FormationError(Exception):
     """
     Formation's base exception. All other exceptions inherit from this.
 
+    This base exception can be caught in a try/except block to catch all custom
+    errors thrown by Formation.
+
+    e.g::
+
+        from formation.exception import FormationError
+        try:
+            # Formation code that may error
+        except FormationError:
+            # Error handling code here
+
     """
     pass
 
 
 class InvalidPropertyError(FormationError):
     """
-    Exception raised when user supplied properties are invalid.
+    Exception raised when user supplied properties are invalid. This happens
+    when an AtomicTemplate contains a required resource-specific parameter
+    which is not supplied by the user. Formation cannot automatically
+    parameterise required resource-specific parameters.
 
     """
     pass
